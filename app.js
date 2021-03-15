7fb8abc4c147ceac831db1efb3c8dcbf93c89739
o0p <rus@gmx.com>
2021-03-15 23:25:50 +0000

    Add getRandSize()

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ for (let i = 0; i < rows * 3; i++) {
     const img = document.createElement('img');
     img.src = `${unsplashURL}`;    
 };
+console.log(getRandSize())
+
+function getRandSize() {
+    return `${getRandNum()}x${getRandNum()}`
+}
 
 function getRandNum() {
     return Math.floor(Math.random() * 10) + 300;

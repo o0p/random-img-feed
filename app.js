da0a6d625e371b278a03ef8537fc22ac73f6e3c4
o0p <rus@gmx.com>
2021-03-15 23:19:51 +0000

    Add getRandNum()

--- app.js
+++ app.js
@@ -6,3 +6,9 @@ for (let i = 0; i < rows * 3; i++) {
     const img = document.createElement('img');
     img.src = `${unsplashURL}`;    
 };
+
+function getRandNum() {
+    return Math.floor(Math.random() * 10) + 300;
+};
+
+
